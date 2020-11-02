510107768300f82e8b4fee911d8cb68bae098252
Martin Tverraen <martin.tverraen@evry.com>
2020-11-03 00:19:30 +0100

    remove serviceport, add hack for containerport

--- src/challenge.ts
+++ src/challenge.ts
@@ -42,13 +42,11 @@ export class Challenge {
         this.dir = dir;
         this.conf = conf;
         this.type = type;
-        
+        /*
         if (conf.exposeHttp){
             conf.ServicePort=conf.exposeHttp[0].containerPort
         }
-        else if (conf.exposeTCP){
-            conf.ServicePort=conf.exposeTCP[0].containerPort
-        }
+        */
     }
 
     static async parse(dir: string) {
